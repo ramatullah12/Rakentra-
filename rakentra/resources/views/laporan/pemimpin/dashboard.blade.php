347220b867efaa3f9d5731881d374759b6a8524d
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-07 08:20:34 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -440,7 +440,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     : defaultData,
 
                 borderColor: '#2563eb',
-
                 backgroundColor: 'rgba(37,99,235,0.15)',
                 fill: true,
                 tension: 0.4,
