a02bd1fa33f9bb2236f9db181834d3729331b6c7
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-08 07:53:56 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -366,7 +366,6 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
                             <span class="text-secondary">
                                 Operasional
                             </span>
-
                             <span class="text-primary fw-bold">
                                 Rp {{ number_format($biayaOperasional,0,',','.') }}
                             </span>
