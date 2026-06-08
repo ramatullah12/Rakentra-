7fefaefaa77c3e677047468f3e832b1007708d74
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-08 07:52:01 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -389,9 +389,7 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
             </div>
 
         </div>
-
     </div>
-
 </div>
 <script>
 document.addEventListener("DOMContentLoaded", function () {
