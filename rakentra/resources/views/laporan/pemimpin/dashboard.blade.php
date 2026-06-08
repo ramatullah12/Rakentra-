2583688a83d8a741f3550039a8614af52e4222b4
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-08 07:55:23 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -353,7 +353,6 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
 
                             <div class="progress-bar bg-success"
                                  style="width:85%"></div>
-
                         </div>
                     </div>
                     <div class="mb-4">
