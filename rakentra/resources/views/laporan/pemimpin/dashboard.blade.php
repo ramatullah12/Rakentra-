003cb2f1495210b5a0063631ae56b6bf0f09632b
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-08 07:54:34 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -358,7 +358,7 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
 
                     </div>
 
-                    {{-- Operasional --}}
+
                     <div class="mb-4">
                         <div class="d-flex justify-content-between mb-2">
                             <span class="text-secondary">
