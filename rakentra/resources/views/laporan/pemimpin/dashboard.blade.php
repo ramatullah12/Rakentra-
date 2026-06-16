200f4e28c44775807ea332f975696bad571ccc75
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-16 09:29:12 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -306,7 +306,7 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
                         Monitoring Sistem
                     </h5>
 
-                    {{-- Maintenance --}}
+
                     <div class="mb-4">
 
                         <div class="d-flex justify-content-between mb-2">
