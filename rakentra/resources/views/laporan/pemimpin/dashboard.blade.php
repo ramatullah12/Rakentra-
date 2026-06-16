148bc0bd524546e07fdbdee67c0ea4f2f7b85f2d
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-16 09:30:28 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -301,7 +301,6 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
                         border-radius:22px;">
 
                 <div class="card-body">
-
                     <h5 class="fw-bold text-white mb-4">
                         Monitoring Sistem
                     </h5>
