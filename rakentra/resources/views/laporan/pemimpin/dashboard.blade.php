c42a4ca7bd41645a7b3ac02193aeeb9298e578e3
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-16 09:32:11 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -288,7 +288,6 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
                     </div>
 
                 </div>
-
             </div>
         </div>
         <div class="col-md-4 mb-4">
