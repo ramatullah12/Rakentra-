bc3c22c97c3e357695f80855e5a060535c067c1b
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-06-17 18:36:06 +0700

    Update dashboard.blade.php

--- rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
+++ rakentra/resources/views/laporan/pemimpin/dashboard.blade.php
@@ -247,7 +247,6 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.m
                            style="font-size:35px;"></i>
 
                     </div>
-
                     <h3 class="fw-bold text-primary">
                         Rp {{ number_format($biayaOperasional,0,',','.') }}
                     </h3>
