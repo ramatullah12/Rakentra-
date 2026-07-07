fb4ce4e6f9708fa5e7a0cab8d3f944b33645284d
ramatullah12 <160828174+ramatullah12@users.noreply.github.com>
2026-07-07 11:43:38 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,6 @@
     </div>
     <div class="row">
         <div class="col-md-3 mb-4">
-
             <div class="card border-0 shadow-sm h-100"
                  style="background:linear-gradient(135deg,#2563eb,#1d4ed8);
                         border-radius:22px;">
